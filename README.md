b41eeb3b1d2f789ed05ab75e9a54e911a97abaa7
Stephan Schwab <sns@caimito.net>
2024-12-22 22:35:03 +0100

    initial import

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# token-weaver
+# Token Weaver
 Magic Link and Access Token library for Java and Spring
+
+- Account database
+- Magic Link emails to login and restore account access
+- Access token as cookie with auto-refresh
+  
---
+++ src/main/java/net/caimito/token_weaver/TokenWeaverApplication.java
@@ -0,0 +1,13 @@
+package net.caimito.token_weaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenWeaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenWeaverApplication.class, args);
+	}
+
+}
